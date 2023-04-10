d3982c929df94ca76e6f67832b73c9e30dc5d8ee
zhangtang <2907578142@qq.com>
2023-04-10 10:06:22 +0800

    format modify

--- enums.d.ts
+++
@@ -1,5 +0,0 @@
-// Auto-generated. Do not edit.
-declare namespace maqueenIRV2 {
-}
-
-// Auto-generated. Do not edit. Really.
--- ws2812b.ts
+++
@@ -1,5 +0,0 @@
-namespace ws2812b {
-    //% shim=sendBufferAsm
-    export function sendBuffer(buf: Buffer, pin: DigitalPin) {
-    }
-}
